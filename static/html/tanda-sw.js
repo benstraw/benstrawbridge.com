14232300d9a4f202f0ce4b677cf79810f4a80d6d
Benjamin Strawbridge <ben@benstrawbridge.com>
2026-03-20 14:09:53 -0700

    make tanda installable offline app

---
+++ static/html/tanda-sw.js
@@ -0,0 +1,51 @@
+const CACHE_NAME = "tanda-memorial-v1";
+const APP_SHELL = [
+  "./",
+  "./Tanda-Tashjian_Light-Light-Limitless-Light.html",
+  "./tanda.webmanifest",
+  "./tanda-og.png",
+  "./tanda-icon-192.png",
+  "./tanda-icon-512.png",
+  "./tanda-maskable-512.png",
+  "./apple-touch-icon.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      return fetch(event.request).then((networkResponse) => {
+        const responseClone = networkResponse.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, responseClone));
+        return networkResponse;
+      }).catch(() => caches.match("./Tanda-Tashjian_Light-Light-Limitless-Light.html"));
+    })
+  );
+});
